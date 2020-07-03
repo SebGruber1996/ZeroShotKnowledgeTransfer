2d097c97910b3ddaec72fea15f178f93743bf714
SebGruber1996 <gruber_sebastian@t-online.de>
2020-07-03 15:44:55 +0200

    not implemented fix for miniimagenet

--- utils/loaders.py
+++ utils/loaders.py
@@ -18,7 +18,7 @@ def visualize(x_norm, dataset):
         mean = torch.Tensor([0.4914, 0.4822, 0.4465]).view(1, 3, 1, 1).to(x_norm.device)
         std = torch.Tensor([0.2023, 0.1994, 0.2010]).view(1, 3, 1, 1).to(x_norm.device)
         x = x_norm * std + mean
-    elif dataset == "Omniglot":
+    elif dataset in ["Omniglot", "MiniImagenet", "CIFAR-FS"]:
         x = x_norm
     else:
         raise NotImplementedError
